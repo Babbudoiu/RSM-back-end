93090971cc7e54909c1143ef6dbfc4090ba8384e
Mattia Biscontini <mattiabiscontini@gmail.com>
2021-08-10 13:03:28 +0100

    added find admin

--- src/admin/admin.controllers.js
+++ src/admin/admin.controllers.js
@@ -9,3 +9,14 @@ exports.createAdmin = async (req,res) => {
         res.status(500).send(error)
     }
 };
+
+exports.findAdmin = async (req, res) => {
+    try {
+       const user = req.params.username;
+       const pass = req.body.password;
+       const targetAdmin = await Admin.findOne({username: user, password: pass}) ;
+       res.status(200).send({ user: targetAdmin, message: "admin found"})
+    } catch (error) {
+        res.status(500).send(error)
+    }
+};
--- src/admin/admin.routes.js
+++ src/admin/admin.routes.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
 const adminRouter = Router();
-const { createAdmin } = require("./admin.controllers");
+const { createAdmin, findAdmin } = require("./admin.controllers");
 
 adminRouter.post("/admin", createAdmin);
+adminRouter.post("/admin/:username", findAdmin);
 
 module.exports = adminRouter;
